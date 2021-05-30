590e293eba405ba7652accccbe3bbd4ba78fa222
Surfincolin <colinwageman@gmail.com>
2021-05-29 19:51:18 -0700

    using baked stimuli, the speller is now running at 60fps

--- gui.py
+++ gui.py
@@ -20,7 +20,6 @@ class PsychoGUI:
 			useRetina=True,
 			fullscr=fullscreen,
 			color=[-1, -1, -1],
-			useFBO=True,
 			gammaErrorPolicy='ignore'
 		);
 
@@ -32,6 +31,7 @@ class PsychoGUI:
 class Letter:
 	def __init__(self, letter, size, position, window):
 		self.letter = letter
+		self.win = window
 		self.stim = visual.TextStim(
 			win=window,
 			text=letter,
@@ -89,6 +89,7 @@ class Speller:
 		dim = math.ceil(len(full_list)**0.5)
 		character_grid = np.reshape(full_list, (dim, dim))
 
+		self.win = window
 		self.ch_ctrl = []
 		ch_ctrl_mat = []
 # 		count = 0
@@ -116,6 +117,8 @@ class Speller:
 
 		self.size = size
 		self.pos = position
+		self.dim = dim
+		self.draw_state = 0
 
 		w, h = window.init_size
 		pi_position = [(w/2) - ltr_size, -(h/2) + ltr_size]
@@ -132,29 +135,95 @@ class Speller:
 			pos = pi_position
 		)
 
-	def create(self):
-		pass
+		self._bake()
 
+	def _bake(self):
+
+		# imgs = []
+		# all_stim = self.ch_ctrl + [self.photoindicator]
+		# for i in range(self.dim * 2):
+		# 	self.raw_update(i)
+		# 	img = visual.BufferImageStim(self.win, all_stim)
+		# 	imgs.append(im)
+
+		# self.raw_reset()
+		# img = visual.BufferImageStim(self.win, all_stim)
+		# imgs.append(im)
+
+		# self.raw_calibrate()
+		# img = visual.BufferImageStim(self.win, all_stim)
+		# imgs.append(im)
+
+		# self.baked_stim = imgs
+		# self.draw_state = len(self.baked_stim) - 2 # off state
+
+		imgs = []
+		for i in range(self.dim * 2):
+			self.win.clearBuffer(color=True, depth=False, stencil=False)
+			self.raw_update(i)
+			self.raw_draw()
+			# self.win.flip()
+			im = self.win._getFrame(buffer='back')
+			imgs.append(im)
+
+		self.win.clearBuffer(color=True, depth=False, stencil=False)
+		self.raw_reset()
+		self.raw_draw()
+		# self.win.flip()
+		im = self.win._getFrame(buffer='back')
+		imgs.append(im)
+
+		self.win.clearBuffer(color=True, depth=False, stencil=False)
+		self.raw_calibrate()
+		self.raw_draw()
+		# self.win.flip()
+		im = self.win._getFrame(buffer='back')
+		imgs.append(im)
+
+		self.baked_stim = []
+		for img in imgs:
+			img_stim = visual.ImageStim( win=self.win, image=img,units='pix', size=[1280,720])
+			self.baked_stim.append(img_stim)
+		self.draw_state = len(self.baked_stim) - 2 # off state
+
+	""" draw, update, rest, calibrate
+
+	Due to slow frame rates, alls stimuli are bake
+	as BufferImageStim. Instead of 72 draw calls,
+	we decrease it to 1.
+	"""
 	def draw(self):
+		self.baked_stim[self.draw_state].draw()
+
+	def update(self, highlight_index):
+		self.draw_state = highlight_index
+
+	def reset(self):
+		self.draw_state = len(self.baked_stim) - 2 # off state
+
+	def calibrate(self):
+		self.draw_state = len(self.baked_stim) - 1 # all on state
+
+	def raw_draw(self):
 		for l in self.ch_ctrl:
 			l.draw()
 		self.photoindicator.draw()
 
-	def update(self, highlight_index):
+	def raw_update(self, highlight_index):
 		for l in self.ch_ctrl:
 			l.off()
 
-		# self.ch_ctrl[highlight_index].on()
 		for ch in self.ch_ctrl_2[highlight_index]:
 			ch.on()
 		self.photoindicator.opacity = 1.0
 
-	def reset(self):
+	def raw_reset(self):
 		for l in self.ch_ctrl: l.off()
 		self.photoindicator.opacity = 0.0
 
-	def calibrate(self):
+	def raw_calibrate(self):
 		for l in self.ch_ctrl: l.on()
+		self.photoindicator.opacity = 1.0
 
 def MsToFrames(ms, fps):
 	return math.ceil(ms/(1000/fps))
@@ -180,9 +249,9 @@ if __name__ == "__main__":
 	f_time = 0.0
 	clock.reset()
 	fps = 60.0
-	frame_on = MsToFrames(125,fps)
+	frame_on = MsToFrames(125 ,fps)
 	frame_off = MsToFrames(250,fps)
-	t = clock.getTime()
+	# t = clock.getTime()
 	while True:		
 		"""
 		FPS
@@ -190,19 +259,14 @@ if __name__ == "__main__":
 		Due to the app only running at 30fps(why???), cutting
 		the standard 125ms~8frames in half to keep 125ms spacing.
 		"""
-		
-		# if mon == 120:
-		# 	mon = -1
-		# 	t = clock.getTime()
-		# 	fps = round((1.0/t)*120.0, 2)
-		# 	gui.win.callOnFlip(clock.reset)
-		# 	print(fps)
+
 
 		cur_time = clock.getTime()
 		elapsed = (cur_time - f_time) * 1000.0
 		if elapsed >= 1000.0/fps:
 			f_time = cur_time
-	
+			t = clock.getTime()
+			
 			# print(round(avg,2))
 			# print(frame_count)
 			# ~250ms 10/60*1000=166.6666
@@ -212,22 +276,30 @@ if __name__ == "__main__":
 			# [1] https://www.frontiersin.org/articles/10.3389/fnhum.2019.00261/full#F1
 			# [2] https://www.frontiersin.org/articles/10.3389/fnhum.2013.00732/full
 			if frame_count == frame_on:
+				# print('on')
 				speller.update(index_count%12)
-				speller.draw()
-				gui.win.flip(clearBuffer=False)
+				# speller.draw()
+				# gui.win.flip(clearBuffer=True)
 
 			if frame_count == frame_off:
-				n = clock.getTime() - t
-				cycle = (n - t) * 1000
-				t = n
-				avg = (avg + cycle) / 2.0
-				print(round(avg,2))
+				# print('off')
 				frame_count = -1
 				speller.reset()
-				speller.draw()
-				gui.win.flip(clearBuffer=False)
+				# speller.draw()
+				# gui.win.flip(clearBuffer=True)
 				index_count += 1
 
+
+			# print(speller.draw_state)
+			speller.draw()
+			gui.win.flip()
+			n = clock.getTime()
+			frame_time = (n - t) * 1000
+			t = n
+			# avg = (avg + frame_time) / 2.0
+			avg = (avg + elapsed) / 2.0
+			# print(round(frame_time,2))
+			# print(frame_count)
 			frame_count += 1
 
 
