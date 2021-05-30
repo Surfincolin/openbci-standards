652e65d0df52440b55367e4461670b548f75fcae
Surfincolin <colinwageman@gmail.com>
2021-05-29 20:29:46 -0700

    cleanup code of excess test/debug junk

--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
 # gui.py
 
 from psychopy import visual, core, event
-# import time
 import math
 import numpy as np
 from pylsl import StreamInfo, StreamOutlet, local_clock
@@ -92,7 +91,7 @@ class Speller:
 		self.win = window
 		self.ch_ctrl = []
 		ch_ctrl_mat = []
-# 		count = 0
+
 		ltr_size = 60
 		w_adj = size[0]-ltr_size
 		h_adj = size[1]-ltr_size
@@ -102,13 +101,13 @@ class Speller:
 		for row in character_grid:
 			r_container = []
 			for ch in row:
-				l = Letter(letter=ch, size=[60,60], position=placement, window=window)
+				l = Letter(letter=ch, size=[ltr_size,ltr_size], position=placement, window=window)
 				self.ch_ctrl.append(l)
 				r_container.append(l)
 				placement = [placement[0] + h_spacing, placement[1]]
 
 			ch_ctrl_mat.append(r_container)
-			placement = [-size[0]/2 + position[0], placement[1] - v_spacing]
+			placement = [-w_adj/2  + position[0], placement[1] - v_spacing]
 
 		# create a list of row and columns for simple indexing 0-12
 		ch_ctrl_mat = np.array(ch_ctrl_mat)
@@ -125,73 +124,53 @@ class Speller:
 		self.photoindicator = visual.Circle(
 			win=window,
 			units="pix",
-			radius=ltr_size/2,
+			radius=ltr_size*(2/3),
 			fillColor=[255,255,255],
 			lineColor=[255,255,255],
 			lineWidth = 1,
 			opacity = 0.0,
 			edges = 32,
-			name = 'off', # Used to determine state
 			pos = pi_position
 		)
 
 		self._bake()
 
 	def _bake(self):
-
-		# imgs = []
-		# all_stim = self.ch_ctrl + [self.photoindicator]
-		# for i in range(self.dim * 2):
-		# 	self.raw_update(i)
-		# 	img = visual.BufferImageStim(self.win, all_stim)
-		# 	imgs.append(im)
-
-		# self.raw_reset()
-		# img = visual.BufferImageStim(self.win, all_stim)
-		# imgs.append(im)
-
-		# self.raw_calibrate()
-		# img = visual.BufferImageStim(self.win, all_stim)
-		# imgs.append(im)
-
-		# self.baked_stim = imgs
-		# self.draw_state = len(self.baked_stim) - 2 # off state
+		"""
+		Creates screenshots of all stimuli states. This allows
+		for faster display changes and the app can reach 60fps.
+		"""
 
 		imgs = []
 		for i in range(self.dim * 2):
 			self.win.clearBuffer(color=True, depth=False, stencil=False)
 			self.raw_update(i)
 			self.raw_draw()
-			# self.win.flip()
 			im = self.win._getFrame(buffer='back')
 			imgs.append(im)
 
 		self.win.clearBuffer(color=True, depth=False, stencil=False)
 		self.raw_reset()
 		self.raw_draw()
-		# self.win.flip()
 		im = self.win._getFrame(buffer='back')
 		imgs.append(im)
 
 		self.win.clearBuffer(color=True, depth=False, stencil=False)
 		self.raw_calibrate()
 		self.raw_draw()
-		# self.win.flip()
 		im = self.win._getFrame(buffer='back')
 		imgs.append(im)
 
 		self.baked_stim = []
 		for img in imgs:
-			img_stim = visual.ImageStim( win=self.win, image=img,units='pix', size=[1280,720])
+			img_stim = visual.ImageStim( win=self.win, image=img,units='pix', size=self.win.init_size)
 			self.baked_stim.append(img_stim)
+
 		self.draw_state = len(self.baked_stim) - 2 # off state
 
-	""" draw, update, rest, calibrate
-
-	Due to slow frame rates, alls stimuli are bake
-	as BufferImageStim. Instead of 72 draw calls,
-	we decrease it to 1.
-	"""
+	# raw_draw == SLOW
+	# draw == FAST
+	# etc.
 	def draw(self):
 		self.baked_stim[self.draw_state].draw()
 
@@ -225,9 +204,16 @@ class Speller:
 		for l in self.ch_ctrl: l.on()
 		self.photoindicator.opacity = 1.0
 
+# ==============
+#  HELPER FNC
+# ==============
 def MsToFrames(ms, fps):
 	return math.ceil(ms/(1000/fps))
 
+
+# ==============
+#  Main Sequence
+# ==============
 if __name__ == "__main__":
 	gui = PsychoGUI(fullscreen=False)
 
@@ -236,71 +222,46 @@ if __name__ == "__main__":
 	# lsl_stream = StreamOutlet(lsl_info)
 
 	clock = core.Clock()
-	opacity = 1.0
-	clock.add(5)
 
-
-	frame_count = 0
-	index_count = 0
-	speller.update(index_count%12)
-	mon = 1
-	# gui.win.callOnFlip(clock.reset)
 	avg = 0
 	f_time = 0.0
 	clock.reset()
 	fps = 60.0
 	frame_on = MsToFrames(125 ,fps)
 	frame_off = MsToFrames(250,fps)
-	# t = clock.getTime()
+	frame_count = 0
+	index_count = 0
+	speller.reset()
+
 	while True:		
-		"""
-		FPS
-
-		Due to the app only running at 30fps(why???), cutting
-		the standard 125ms~8frames in half to keep 125ms spacing.
-		"""
-
 
 		cur_time = clock.getTime()
 		elapsed = (cur_time - f_time) * 1000.0
 		if elapsed >= 1000.0/fps:
 			f_time = cur_time
-			t = clock.getTime()
 			
-			# print(round(avg,2))
-			# print(frame_count)
+			# notes:
 			# ~250ms 10/60*1000=166.6666
 			# 125ms -> 62.5ms or 125ms  capture 800ms
 			# Fz, Cz, Pz, Oz, P3, P4, PO7 and PO8
 			# referenced to the right earlobe and grounded to the left mastoid
 			# [1] https://www.frontiersin.org/articles/10.3389/fnhum.2019.00261/full#F1
 			# [2] https://www.frontiersin.org/articles/10.3389/fnhum.2013.00732/full
+
+
 			if frame_count == frame_on:
-				# print('on')
 				speller.update(index_count%12)
-				# speller.draw()
-				# gui.win.flip(clearBuffer=True)
 
 			if frame_count == frame_off:
-				# print('off')
-				frame_count = -1
 				speller.reset()
-				# speller.draw()
-				# gui.win.flip(clearBuffer=True)
+				frame_count = -1
 				index_count += 1
 
-
-			# print(speller.draw_state)
 			speller.draw()
 			gui.win.flip()
-			n = clock.getTime()
-			frame_time = (n - t) * 1000
-			t = n
-			# avg = (avg + frame_time) / 2.0
-			avg = (avg + elapsed) / 2.0
-			# print(round(frame_time,2))
-			# print(frame_count)
+			
 			frame_count += 1
+			avg = (avg + elapsed) / 2.0
 
 
 		if len(event.getKeys())>0:
@@ -310,7 +271,7 @@ if __name__ == "__main__":
 
 
 	total = round(avg,2)
-	print('update:', total, 'ms')
+	print('Average Frame Duration:', total, 'ms')
 	gui.win.close()
 	core.quit()
 	
